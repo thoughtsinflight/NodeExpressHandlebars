a5b9ec6c0a7d713301be63530592eda5ad33cdf3
Thomas Beaman <thomasthekingmaker@gmail.com>
2019-12-19 12:22:29 -0500

    schema update for primary key

--- schema.sql
+++ schema.sql
@@ -4,7 +4,7 @@ USE burgers_db;
 
 CREATE TABLE burgers
 (
-	id int NOT NULL AUTO_INCREMENT,
+	id int NOT NULL AUTO_INCREMENT primary key,
 	burger varchar(255) NOT NULL,
 	devoured BOOLEAN default 0
 )
